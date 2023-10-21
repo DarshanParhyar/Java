9593dbee76f6e21c2e90bf121e7b2482412cc962
Darshan <darshankumar.parhyar@gmail.com>
2023-10-21 11:59:34 +0500

    Implement widening conversion

---
+++ ImplicitTypeConversion.java
@@ -0,0 +1,10 @@
+public class ImplicitTypeConversion
+{
+    public static void main(String[] args) {
+        int num=4;
+        float fNum=num;
+        System.out.println(fNum);
+        System.out.print(num);
+
+    }
+}
