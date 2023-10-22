85c4097e551b940f2bf1a4ded5f0bc7ddd1e42cb
Darshan <darshankumar.parhyar@gmail.com>
2023-10-22 22:28:21 +0500

    Implemented explicit type conversion

---
+++ ExplicitTypeConversion.java
@@ -0,0 +1,9 @@
+public class ExplicitTypeConversion
+{
+    public static void main(String[] args) {
+        float fNum = 15.265f;
+        int iNum = (int)fNum;
+        System.out.print(iNum);
+    }
+
+}
