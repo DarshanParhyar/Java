25509bf465677bffa0db5105016e1743c04617bd
Darshan <darshankumar.parhyar@gmail.com>
2023-10-23 23:08:13 +0500

    implemented functionality of replace string

---
+++ ReplaceString.java
@@ -0,0 +1,14 @@
+
+public class ReplaceString {
+
+    public static void main (String [] args)
+    {
+       
+         
+          
+            String line = "The quick brown fox jumps over the lazy dog.";
+            String replaceLine = line.replace("dog","Fox");
+
+        System.out.print(replaceLine);
+    }
+}
