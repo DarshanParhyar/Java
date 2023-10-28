bfd1db9ec2ec2ebb551fe0cdb8d9ae622cd0a00a
Darshan <darshankumar.parhyar@gmail.com>
2023-10-28 18:27:58 +0500

    Implemented Assignment Operations

---
+++ AssignmentOperatorsPractice.java
@@ -0,0 +1,14 @@
+public class AssignmentOperatorsPractice {
+    public static void main(String[] args) {
+        int x = 10;
+        x += 5;
+        System.out.println("After x+: " + x);
+        x -= 1;
+        System.out.println("After x-: " + x);
+        x *= 3;
+        System.out.println("After x*: " + x);
+        x /= 2;
+        System.out.println("After x/: " + x);
+
+    }
+}
