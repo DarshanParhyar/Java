4ce870db50b4df492e3553493817f437abb81464
Darshan <darshankumar.parhyar@gmail.com>
2023-10-28 18:26:54 +0500

    Implemented arithmetic operations

---
+++ ArithmeticOperatorsPractice.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperatorsPractice {
+    public static void main(String[] args) {
+        double num1 = 14.4, num2 = 4.5, result;
+        result = num1 + num2;
+        System.out.println("Addition of two numbers: " + result);
+        result = num1 - num2;
+        System.out.println("Subtraction of two numbers: " + result);
+        result = num1 * num2;
+        System.out.println("Multiplication of two numbers: " + result);
+        result = num1 / num2;
+        System.out.println("Division of two numbers: " + result);
+    }
+}
