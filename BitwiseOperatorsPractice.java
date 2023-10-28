2f83a17e671211b67fd4c1e99939c5f8d0194a30
Darshan <darshankumar.parhyar@gmail.com>
2023-10-28 18:30:00 +0500

    implemented bitwise operations

---
+++ BitwiseOperatorsPractice.java
@@ -0,0 +1,10 @@
+public class BitwiseOperatorsPractice {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        System.out.println("a & b :" + (a & b));
+        System.out.println("a | b :" + (a | b));
+        System.out.println("a ^ b :" + (a ^ b));
+        System.out.println("~a :" + (~a));
+    }
+}
